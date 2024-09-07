ed3047b4820c938200d43dc1a7b9a72ba8b265dd
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-09-07 16:52:45 -0400

    making search bar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-export function Header({ownerName, repoName}) {
+import SearchBar from "./SearchBar";
+export function Header({ ownerName, repoName }) {
     return (
-        <>
-            <div className="flex p-[10px]">
+        <div className="flex justify-between p-[10px]">
+            <div className="flex">
                 <div className="border p-[5px]">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -16,6 +17,9 @@ export function Header({ownerName, repoName}) {
                     {ownerName} / <span className="font-bold">{repoName}</span>
                 </div>
             </div>
-        </>
+            <div className="ml-auto">
+                <SearchBar/>
+            </div>
+        </div>
     );
 }
