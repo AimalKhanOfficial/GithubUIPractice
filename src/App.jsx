38265c09ccdb60250d1cfb8c4ccfac7c568931b5
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-09-07 18:25:22 -0400

    changing layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,16 @@ function App() {
   return (
     <>
       <Header ownerName={'AimalKhanOfficial'} repoName={'GithubUIPractice'} />
-      <div className="w-[70%] m-auto mt-[20px]">
+      <div className="lg:w-[70%] lg:m-auto lg:mt-[20px] p-[10px]">
         <RepoActions repoName={'GithubUIPractice'} />
-        <BranchInfoHeader />
+        <div className="flex justify-between lg:mt-[20px]">
+          <div className="w-full lg:w-[70%]">
+            <BranchInfoHeader />
+          </div>
+          <div className="hidden lg:block lg:w-[30%] border ml-[5px] p-[5px]">
+            <p>SideBar</p>
+          </div>
+        </div>
       </div>
     </>
   )
--- src/components/BranchInfoHeader.jsx
+++ src/components/BranchInfoHeader.jsx
@@ -4,24 +4,26 @@ import SearchBar from "./SearchBar";
 export default function BranchInfoHeader() {
     return (
         <>
-            <div className="flex justify-between pb-[15px] mt-[20px]">
+            <div className="flex justify-between pb-[15px]">
                 <div className="flex">
                     <select className="bg-gray-200 text-black rounded-lg p-[5px] items-center" name="branches">
                         <option value="main">main</option>
                         <option value="cool-feature">cool-feature</option>
                         <option value="cool-feature">cool-feature</option>
                     </select>
-                    <p className="ml-[7px] mt-[5px]">
+                    <p className="ml-[7px] mt-[5px] hidden lg:block">
                         <span className="font-bold">3</span> Branch
                     </p>
-                    <p className="ml-[7px] mt-[5px]">
-                    <span className="font-bold">0</span> Tags
+                    <p className="ml-[7px] mt-[5px] hidden lg:block">
+                        <span className="font-bold">0</span> Tags
                     </p>
                 </div>
                 <div className="flex justify-between">
-                    <SearchBar placeHolder={'Go to File'}/>
-                    <IconWithText svgUrl={'/down.svg'} text={'Add File'} addBorder={true}/>
-                    <IconWithText svgUrl={'/code.svg'} text={'Code'} addBorder={true}/>
+                    <div className="hidden lg:flex">
+                        <SearchBar placeHolder={'Go to File'} />
+                        <IconWithText svgUrl={'/down.svg'} text={'Add File'} addBorder={true} />
+                    </div>
+                    <IconWithText svgUrl={'/code.svg'} text={'Code'} addBorder={true} />
                 </div>
             </div>
         </>
